51bc35a712da99aa007b85f33cb55d91b5d544a7
PFSymphony <pf.vilquin@symphony.com>
2019-01-03 17:42:52 +0900

    Forces use of TLS1.2 (Requirement since Symphony 1.52)

--- UpdateAppEntitlements/UpdateAppEntitlements.ps1
+++ UpdateAppEntitlements/UpdateAppEntitlements.ps1
@@ -32,6 +32,9 @@
 
     # This script was designed to read a csv file of user ids with one id per line and update the user for Dow Jones Premium. 
 
+#Forces PowerShell to use TLS 1.2 as a minimum (requirement since Symphony 1.52)
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+ 
 # Fill in these Variables
 
     $certificateFileAndPath =     "C:\mycerts\bot.user1.p12"
